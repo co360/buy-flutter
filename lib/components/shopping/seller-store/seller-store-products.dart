d105f4cced93c051f4ec2d0ec7802c9b1333ed49
Ammar <armmarov@gmail.com>
2020-07-30 07:22:43 +0800

    Remove unnecessary item

--- lib/components/shopping/seller-store/seller-store-products.dart
+++ lib/components/shopping/seller-store/seller-store-products.dart
@@ -4,11 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:storeFlutter/models/identity/company-profile.dart';
 import 'package:storeFlutter/models/identity/company.dart';
 import 'package:storeFlutter/models/shopping/product.dart';
-import 'package:storeFlutter/models/filter-type.dart';
 import 'package:storeFlutter/blocs/shopping/product-listing-bloc.dart';
 import 'package:storeFlutter/components/shopping/product-listing/product-listing-grid.dart';
 import 'package:storeFlutter/components/app-general-error-info.dart';
-import 'package:storeFlutter/services/product-service.dart';
 
 class SellerStoreProducts extends StatelessWidget {
   final Company sellerCompany;
@@ -20,21 +18,6 @@ class SellerStoreProducts extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   Map<String, FilterValue> filter = {
-  //     "companyId": FilterValue(
-  //         sellerCompany.id.toString(), sellerCompany.id.toString(), 0)
-  //   };
-  //   return BlocProvider<ProductListingBloc>(
-  //     create: (context) => ProductListingBloc()
-  //       ..add(ProductListingSearch(ProductListingQueryFilter(
-  //           query: "", filters: filter != null ? filter : {}))),
-  //     child: Builder(builder: (context) {
-  //       return buildChild(context);
-  //     }),
-  //   );
-  // }
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ProductListingBloc, ProductListingState>(
