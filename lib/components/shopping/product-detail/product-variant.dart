4439296d54754e160d01b2d869140bbb209f372c
teybhacc <dylantey97@gmail.com>
2020-07-30 16:54:02 +0800

    Product options using real data.

--- lib/components/shopping/product-detail/product-variant.dart
+++ lib/components/shopping/product-detail/product-variant.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:storeFlutter/models/shopping/product.dart';
+import 'package:storeFlutter/models/shopping/sku.dart';
+import 'package:storeFlutter/models/shopping/variant-type-value.dart';
 import 'package:storeFlutter/models/shopping/variant-type.dart';
+import 'package:storeFlutter/models/shopping/variant-value.dart';
 import 'package:storeFlutter/services/variant-type-service.dart';
 import 'package:storeFlutter/util/app-theme.dart';
 
@@ -17,7 +20,7 @@ class ProductVariant extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<VariantOption>>(
-      future: buildVariantOptionsMock(),
+      future: buildVariantOptions(),
       builder:
           (BuildContext context, AsyncSnapshot<List<VariantOption>> snapshot) {
         return Column(
@@ -78,32 +81,116 @@ class ProductVariant extends StatelessWidget {
 
   Future<List<VariantOption>> buildVariantOptions() async {
     // TODO copy from angular product-variant.component.ts?
-    print("buildvariantoptions ${product.category.variantFamily.variantTypes}");
 //    int index = 0;
 
     List<VariantOption> variantOptions = [];
+    List<Sku> variantSkus = product.variantSkus;
     for (var i = 0;
         i < product.category.variantFamily.variantTypes.length;
         i++) {
-      VariantType temp = product.category.variantFamily.variantTypes[i];
-
-      VariantType type = await _variantTypeService.get(temp.id);
-      print(
-          "return from service ${type.name}, ${type.code}, ${type.variantTypeValues}");
+      VariantType type = product.category.variantFamily.variantTypes[i];
 
       var option = VariantOption();
+      if (i > 0) {
+        option.parentOption = variantOptions[i - 1];
+      }
+
+//      VariantType type = await _variantTypeService.get(temp.id);
+
       option.variantType = type;
 
-      type.variantTypeValues.forEach((vtv) {
-        print("variant type value $vtv");
-        option.labels.add(vtv.name);
-        option.values.add(vtv.value);
+      variantSkus.forEach((sku) {
+        if (option.parentOption == null) {
+          final variantValue = sku.variantValues[i];
+          VariantTypeValue temp = variantValue.variantType.variantTypeValues
+              .firstWhere((vv) => vv.id.toString() == variantValue.value);
+          if (temp == null && !(variantValue.value is int)) {
+            temp =
+                VariantTypeValue(0, variantValue.label, variantValue.value, 0);
+          }
+          if (temp != null && !option.values.contains(temp.value)) {
+            option.values.add(temp.value);
+            if (variantValue.label != null) {
+              option.labels.add(variantValue.label);
+            } else {
+              if (variantValue.variantType != null &&
+                  variantValue.variantType.variantTypeValues != null) {
+                variantValue.variantType.variantTypeValues.forEach((v) {
+                  if (variantValue.value == v.value.toString()) {
+                    option.labels.add(v.name);
+                  }
+                });
+              }
+            }
+          }
+        } else {
+          final parentValue = sku.variantValues[i - 1];
+          final variantValue = sku.variantValues[i];
+
+          VariantTypeValue tempParent;
+          tempParent = parentValue.variantType.variantTypeValues.firstWhere(
+              (finder) => finder.id.toString() == parentValue.value);
+          if (tempParent == null && !(parentValue.value is int)) {
+            tempParent =
+                VariantTypeValue(0, parentValue.label, parentValue.value, 0);
+          }
+          VariantTypeValue temp;
+          temp = variantValue.variantType.variantTypeValues.firstWhere(
+              (finder) => finder.id.toString() == variantValue.value);
+          if (temp == null && !(variantValue.value is int)) {
+            temp =
+                VariantTypeValue(0, variantValue.label, variantValue.value, 0);
+          }
+
+          //if parent selected
+          if (option.parentOption.selectedValue != null) {
+            if (tempParent.value == option.parentOption.selectedValue &&
+                !option.values.contains(temp.value)) {
+              option.values.add(temp.value);
+              if (variantValue.label != null) {
+                option.labels.add(variantValue.label);
+              } else {
+                if (variantValue.variantType != null &&
+                    variantValue.variantType.variantTypeValues != null) {
+                  variantValue.variantType.variantTypeValues.forEach((v) {
+                    if (variantValue.value.toString() == v.value) {
+                      option.labels.add(v.name);
+                    }
+                  });
+                }
+                // option.labels.push(variantValue.value);
+              }
+            }
+          } else {
+            if (!option.values.contains(temp.value)) {
+              option.values.add(temp.value);
+              if (variantValue.label != null) {
+                option.labels.add(variantValue.label);
+              } else {
+                if (variantValue.variantType != null &&
+                    variantValue.variantType.variantTypeValues != null) {
+                  variantValue.variantType.variantTypeValues.forEach((v) {
+                    if (variantValue.value.toString() == v.value) {
+                      option.labels.add(v.name);
+                    }
+                  });
+                }
+                // option.labels.push(variantValue.value);
+              }
+            }
+          }
+        }
       });
+
+//      type.variantTypeValues.forEach((vtv) {
+//        print("variant type value $vtv");
+//        option.labels.add(vtv.name);
+//        option.values.add(vtv.value);
+//      });
+//      variantOptions.add(option);
       variantOptions.add(option);
-      print("print labels ${option.labels}");
     }
 
-    print("before return ${variantOptions}");
     return variantOptions;
   }
 }
@@ -126,8 +213,8 @@ class VariantOptionWidget extends StatelessWidget {
         Wrap(
           spacing: 10,
           runSpacing: 10,
-          children: option.values.map((e) {
-            return VariantOptionValueWidget(e);
+          children: option.labels.map((e) {
+            return VariantOptionValueWidget(e, option);
           }).toList(),
         ),
       ],
@@ -137,8 +224,9 @@ class VariantOptionWidget extends StatelessWidget {
 
 class VariantOptionValueWidget extends StatelessWidget {
   final String value;
+  final VariantOption option;
 
-  VariantOptionValueWidget(this.value);
+  VariantOptionValueWidget(this.value, this.option);
 
   @override
   Widget build(BuildContext context) {
